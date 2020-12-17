1c73f88bbf2da202b0667850648c1c680d856306
diegoelcairo <diegosastoque97@gmail.com>
2020-12-17 15:59:22 -0500

    local changes

---
+++ Semestral/GlobalVariables.vb
@@ -0,0 +1,24 @@
+﻿Public Class GlobalVariables
+    Public Shared userId As Integer
+    Public Shared userName As String
+
+    Public Property user() As Integer
+        Get
+            Return userId
+        End Get
+        Set(ByVal value As Integer)
+            userId = value
+        End Set
+    End Property
+
+    Public Property name() As Integer
+        Get
+            Return userName
+        End Get
+        Set(ByVal value As Integer)
+            userName = value
+        End Set
+    End Property
+
+
+End Class
